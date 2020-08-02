45ed997c47fe084ca967c289545c5143da65cd4c
Rodrigo Alvarez <42115805+roalvarez@users.noreply.github.com>
2020-08-02 20:06:18 -0300

    scale value in alternative format

--- script.js
+++ script.js
@@ -58,7 +58,7 @@ window.onload = () => {
             let placeText = document.createElement('a-link');
             placeText.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
             placeText.setAttribute('title', place.name);
-            placeText.setAttribute('scale', '15 15 15');
+            placeText.setAttribute('scale', {x: 15, y: 15, z: 15});
 
             placeText.addEventListener('loaded', () => {
                 window.dispatchEvent(new CustomEvent('gps-entity-place-loaded'))
